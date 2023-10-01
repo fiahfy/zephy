00967fc669c930d024d95d4cf23e97bf8010278a
fiahfy <d.fiahfy@gmail.com>
2023-10-01 15:59:55 +0900

    Refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,123 @@
-import '@fontsource/roboto/300.css'
-import '@fontsource/roboto/400.css'
-import '@fontsource/roboto/500.css'
-import '@fontsource/roboto/700.css'
-import Layout from '~/components/Layout'
-import { DragGhostProvider } from '~/contexts/DragGhostContext'
-import { StoreProvider } from '~/contexts/StoreContext'
-import { ThemeProvider } from '~/contexts/ThemeContext'
-import { TrafficLightsProvider } from '~/contexts/TrafficLightsContext'
-import { WatcherProvider } from '~/contexts/WatcherContext'
-import IndexPage from '~/pages'
+import { Box, GlobalStyles, Toolbar } from '@mui/material'
+import { useMemo } from 'react'
+import AddressBar from '~/components/AddressBar'
+import Explorer from '~/components/Explorer'
+import Inspector from '~/components/Inspector'
+import Navigator from '~/components/Navigator'
+import Settings from '~/components/Settings'
+import Sidebar from '~/components/Sidebar'
+import StatusBar from '~/components/StatusBar'
+import useCurrentTitle from '~/hooks/useCurrentTitle'
+import useEventListener from '~/hooks/useEventListener'
+import useMessageListener from '~/hooks/useMessageListener'
+import useTitle from '~/hooks/useTitle'
+import { createMenuHandler } from '~/utils/contextMenu'
+import { useAppSelector } from '~/store'
+import { selectCurrentDirectory } from '~/store/window'
 
 const App = () => {
+  const currentDirectory = useAppSelector(selectCurrentDirectory)
+
+  useEventListener()
+  useMessageListener()
+  const title = useCurrentTitle()
+  useTitle(title)
+
+  const Component = useMemo(() => {
+    switch (currentDirectory) {
+      case 'zephy://settings':
+        return Settings
+      default:
+        return Explorer
+    }
+  }, [currentDirectory])
+
+  const handleContextMenu = useMemo(() => createMenuHandler(), [])
+
   return (
-    <StoreProvider>
-      <ThemeProvider>
-        <TrafficLightsProvider>
-          <WatcherProvider>
-            <DragGhostProvider>
-              <Layout>
-                <IndexPage />
-              </Layout>
-            </DragGhostProvider>
-          </WatcherProvider>
-        </TrafficLightsProvider>
-      </ThemeProvider>
-    </StoreProvider>
+    <Box
+      onContextMenu={handleContextMenu}
+      sx={{
+        display: 'flex',
+        height: '100%',
+        overflow: 'hidden',
+        userSelect: 'none',
+      }}
+    >
+      <GlobalStyles
+        styles={{
+          'html, body, #root': {
+            height: '100%',
+          },
+          '::-webkit-scrollbar': {
+            width: 10,
+            height: 10,
+            '&-corner': {
+              backgroundColor: 'transparent',
+            },
+          },
+          '.theme-light': {
+            '& ::-webkit-scrollbar-thumb': {
+              backgroundColor: '#e0e0e0',
+              '&:hover': {
+                backgroundColor: '#d2d2d2',
+              },
+              '&:active': {
+                backgroundColor: '#bdbdbd',
+              },
+            },
+          },
+          '.theme-dark': {
+            '& ::-webkit-scrollbar-thumb': {
+              backgroundColor: '#424242',
+              '&:hover': {
+                backgroundColor: '#505050',
+              },
+              '&:active': {
+                backgroundColor: '#616161',
+              },
+            },
+          },
+          '.col-resizing *': {
+            cursor: 'col-resize',
+            userSelect: 'none',
+          },
+        }}
+      />
+      <AddressBar />
+      <Sidebar variant="primary">
+        <Navigator />
+      </Sidebar>
+      <Box
+        component="main"
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          flexGrow: 1,
+          minWidth: 0,
+        }}
+      >
+        <Toolbar
+          sx={{
+            flexShrink: 0,
+            minHeight: (theme) => `${theme.mixins.addressBar.height}!important`,
+          }}
+        />
+        <Box sx={{ flexGrow: 1, overflow: 'auto' }}>
+          <Component />
+        </Box>
+        <Toolbar
+          sx={{
+            flexShrink: 0,
+            minHeight: (theme) => `${theme.mixins.statusBar.height}!important`,
+          }}
+        />
+      </Box>
+      <Sidebar variant="secondary">
+        <Inspector />
+      </Sidebar>
+      <StatusBar />
+    </Box>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '~/App'
+import { DragGhostProvider } from '~/contexts/DragGhostContext'
+import { StoreProvider } from '~/contexts/StoreContext'
+import { ThemeProvider } from '~/contexts/ThemeContext'
+import { TrafficLightsProvider } from '~/contexts/TrafficLightsContext'
+import { WatcherProvider } from '~/contexts/WatcherContext'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <StoreProvider>
+      <ThemeProvider>
+        <TrafficLightsProvider>
+          <DragGhostProvider>
+            <WatcherProvider>
+              <App />
+            </WatcherProvider>
+          </DragGhostProvider>
+        </TrafficLightsProvider>
+      </ThemeProvider>
+    </StoreProvider>
   </React.StrictMode>,
 )
 
