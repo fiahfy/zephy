dbf010d85f6882bd0307739957c735a4f9def4af
fiahfy <d.fiahfy@gmail.com>
2024-05-26 18:16:27 +0900

    Update worker.ts

--- electron/worker.ts
+++ electron/worker.ts
@@ -17,47 +17,59 @@ parentPort?.on('message', async (workerData) => {
   switch (type) {
     case 'copyEntries': {
       const entry = await copyEntries(data[0], data[1])
-      return parentPort?.postMessage(entry)
+      parentPort?.postMessage(entry)
+      break
     }
     case 'createDirectory': {
       const entry = await createDirectory(data[0])
-      return parentPort?.postMessage(entry)
+      parentPort?.postMessage(entry)
+      break
     }
     case 'createThumbnailUrl': {
       const entry = await createThumbnailUrl(data[0], data[1])
-      return parentPort?.postMessage(entry)
+      parentPort?.postMessage(entry)
+      break
     }
     case 'getDetailedEntries': {
       const entries = await getDetailedEntries(data[0])
-      return parentPort?.postMessage(entries)
+      parentPort?.postMessage(entries)
+      break
     }
     case 'getDetailedEntriesForPaths': {
       const entries = await getDetailedEntriesForPaths(data[0])
-      return parentPort?.postMessage(entries)
+      parentPort?.postMessage(entries)
+      break
     }
     case 'getDetailedEntry': {
       const entry = await getDetailedEntry(data[0])
-      return parentPort?.postMessage(entry)
+      parentPort?.postMessage(entry)
+      break
     }
     case 'getEntries': {
       const entries = await getEntries(data[0])
-      return parentPort?.postMessage(entries)
+      parentPort?.postMessage(entries)
+      break
     }
     case 'getMetadata': {
       const metadata = await getMetadata(data[0])
-      return parentPort?.postMessage(metadata)
+      parentPort?.postMessage(metadata)
+      break
     }
     case 'getRootEntry': {
       const entry = await getRootEntry(data[0])
-      return parentPort?.postMessage(entry)
+      parentPort?.postMessage(entry)
+      break
     }
     case 'moveEntries': {
       const entries = await moveEntries(data[0], data[1])
-      return parentPort?.postMessage(entries)
+      parentPort?.postMessage(entries)
+      break
     }
     case 'renameEntry': {
       const entry = await renameEntry(data[0], data[1])
-      return parentPort?.postMessage(entry)
+      parentPort?.postMessage(entry)
+      break
     }
   }
+  process.exit()
 })
